0ad5b09e6a1576a01dd5c0608f3d69905f419c56
RobotLelick <122119082+RobotLelick@users.noreply.github.com>
2023-01-09 20:08:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from board import Board
 from tank import Tank
+from game_map import map_board
 from wall import Wall
+FPS = 60
+SPEED = 20
 
 if __name__ == '__main__':
     pygame.init()
@@ -9,12 +12,19 @@ if __name__ == '__main__':
     size = 650, 650
     screen = pygame.display.set_mode(size)
     bg = pygame.image.load("data/пол.png")
-    board = Board(6, 6)
-    FPS = 60
-    speed = 20
+    board = Board(10, 10)
+
     all_sprites = pygame.sprite.Group()
-    player = Tank()
-    all_sprites.add(player)
+    wall_sprites = pygame.sprite.Group()
+    for i in range(len(map_board)):
+        for j in range(len(map_board)):
+            if map_board[i][j] == 1:
+                player = Tank(j * 65, i * 65)
+                all_sprites.add(player)
+            if map_board[i][j] == 2:
+                obj = Wall(j * 65, i * 65)
+                wall_sprites.add(obj)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -22,19 +32,19 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    Tank.move(all_sprites)
+                    player.move(0, -1, player.number_cell())
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
-                    Tank.move(all_sprites)
+                    player.move(0, 1, player.number_cell())
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    Tank.move(all_sprites)
+                    player.move(-1, 0, player.number_cell())
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
-                    Tank.move(all_sprites)
+                    player.move(1, 0, player.number_cell())
         screen.blit(bg, (0, 0))
         all_sprites.update()
         all_sprites.draw(screen)
-        board.render(screen)
+        wall_sprites.draw(screen)
         pygame.display.flip()
     pygame.quit()
