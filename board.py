8e6d93345c0a51aeb54c28ae5c72d94215a26477
RobotLelick <122119082+RobotLelick@users.noreply.github.com>
2023-01-09 20:09:59 +0300

    Update board.py

--- board.py
+++ board.py
@@ -5,12 +5,6 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[0] * width for _ in range(height)] # massiv
-        self.board[0][0] = 1 # позиция игрока(0 пусто, 1 игрок, 2 противник, 3 стена)
         self.left = 0
         self.top = 0
-        self.cell_size = 100
-
-    def render(self, screen):
-        # сюда вставишь update - ы своего класса
-        pass
+        self.cell_size = 65
