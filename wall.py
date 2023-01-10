953e63204ccd99aaf476f07f925dfae2684e5d48
RobotLelick <122119082+RobotLelick@users.noreply.github.com>
2023-01-10 20:49:57 +0300

    Update wall.py

--- wall.py
+++ wall.py
@@ -3,7 +3,7 @@ import pygame
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self, x, y)
         self.wall = pygame.image.load("data/стена.png").convert_alpha()
         self.image = self.wall
         self.rect = self.wall.get_rect()
